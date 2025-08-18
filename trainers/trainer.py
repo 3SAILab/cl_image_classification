d9d3caff8e2d3eb61f5b53883f0fc620ef6df1c5
HedyyyChen <1298999619@qq.com>
2025-08-18 13:56:14 +0800

    新增acc,val_loss

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -25,7 +25,7 @@ def trainer(device, model, need_seed=False):
 
     # 设置数据集路径
     data_root = os.path.abspath(os.getcwd())
-    image_path = os.path.join(data_root, "data")
+    image_path = os.path.join(data_root, "data", "my_data")
     assert os.path.exists(image_path),"file {} does not exist.".format(image_path)
 
     # 导入相关参数
@@ -66,13 +66,20 @@ def trainer(device, model, need_seed=False):
 
     print("using {} images for training, using {} images for validation.".format(train_num, val_num))
 
+    # 类别标签写入json文件
+    image_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in image_list.items())
+    json_str = json.dumps(cla_dict, indent=4)
+    with open("configs/my_class_indices.json","w") as json_file:
+        json_file.write(json_str)
+
     # 导入模型
     net = model(model_config)
     net.to(device)
     total_params = sum(p.numel() for p in net.parameters())
 
     # 设置损失函数和优化器
-    loss_function = torch.nn.CrossEntropyLoss()
+    loss_function = torch.nn.CrossEntropyLoss(label_smoothing=0.1)
     optimizer = optim.Adam(net.parameters(), lr = 0.0001)
 
     # 训练
@@ -80,54 +87,75 @@ def trainer(device, model, need_seed=False):
     save_name = "{}.pth".format(model.__name__)
     save_path = os.path.join(data_root, "checkpoints", save_name)
     train_steps = len(train_loader)
+    val_steps = len(val_loader)
     loss_list = []
     accuracy_list = []
+    val_loss_list = []
+    val_accuracy_list = []
     total_train_start = time.perf_counter()
     
     for epoch in range(epochs):
         net.train()
         epoch_train_start = time.perf_counter()
         running_loss = 0.0
+        running_acc = 0.0
         train_bar = tqdm(train_loader, file = sys.stdout)
         for step,data in enumerate(train_bar):
             images,labels = data
             optimizer.zero_grad()
             if model.__name__ == "GoogLeNet":
                 logits, aux_logits1, aux_logits2 = net(images.to(device))
+                train_predict_y = torch.max(logits, dim=1)[1]
                 loss0 = loss_function(logits, labels.to(device))
                 loss1 = loss_function(aux_logits1, labels.to(device))
                 loss2 = loss_function(aux_logits2, labels.to(device))
                 loss = loss0 + loss1*0.3 + loss2*0.3
+            elif model.__name__ == "Inception3":
+                logits, aux_logits = net(images.to(device))
+                train_predict_y = torch.max(logits, dim=1)[1]
+                loss0 = loss_function(logits, labels.to(device))
+                loss1 = loss_function(aux_logits, labels.to(device))
+                loss = loss0 + loss1*0.3
             else:
                 outputs = net(images.to(device))
+                train_predict_y = torch.max(outputs, dim=1)[1]
                 loss = loss_function(outputs, labels.to(device))
+                
+            acc = torch.eq(train_predict_y, labels.to(device)).sum().item()
+            
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
+            running_acc += acc
             train_bar.desc = "train epoch [{}/{}] loss: {:.3f}".format(epoch + 1,
                                                                        epochs,
                                                                        loss)
 
         net.eval()
-        acc = 0.0
+        val_acc = 0.0
+        val_l = 0.0
         with torch.no_grad():
             val_bar = tqdm(val_loader, file=sys.stdout)
             for val_data in val_bar:
                 images, labels = val_data
                 outputs = net(images.to(device))
+                val_l += loss_function(outputs, labels.to(device)).item()
                 predict_y = torch.max(outputs, dim=1)[1]
-                acc += torch.eq(predict_y, labels.to(device)).sum().item()
+                val_acc += torch.eq(predict_y, labels.to(device)).sum().item()
 
         epoch_train_end = time.perf_counter()
-        val_accuracy = acc / val_num
+        val_accuracy = val_acc / val_num
+        val_loss = val_l / val_steps
         print("epoch: {} train_loss: {:.3f} val_accuracy: {:.3f} time: {:.5f}".format(epoch + 1,
                                                                          running_loss / train_steps,
                                                                          val_accuracy,
                                                                          epoch_train_end - epoch_train_start))
 
         loss_list.append(running_loss / train_steps)
-        accuracy_list.append(val_accuracy)
+        accuracy_list.append(running_acc / train_num)
+        val_loss_list.append(val_loss)
+        val_accuracy_list.append(val_accuracy)
 
         if best_acc < val_accuracy:
             best_acc = val_accuracy
@@ -148,7 +176,7 @@ def trainer(device, model, need_seed=False):
     # 绘制准确率曲线
     acc_save_name = "{}_acc.jpg".format(model.__name__)
     acc_save_path = os.path.join(data_root, "results", acc_save_name)
-    plot_accuracy_curves(accuracy_list, epochs, acc_save_path)
+    plot_accuracy_curves(val_accuracy_list, epochs, acc_save_path)
 
     # 保存训练日志
     train_log_name = "{}.txt".format(model.__name__)
@@ -161,8 +189,10 @@ def trainer(device, model, need_seed=False):
         "Initial Learning Rate": 0.0001,
         "Best Accuracy": best_acc,
         "Train Time": total_train_time,
-        "Train Loss List": loss_list,
-        "Accuracy List": accuracy_list
+        "Loss List": loss_list,
+        "Accuracy List": accuracy_list,
+        "Val Loss List": val_loss_list,
+        "Val Accuracy List": val_accuracy_list
     }
 
     with open(train_log_path, "w", encoding="utf-8") as f:
