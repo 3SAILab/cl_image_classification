bd681bddab1ac52179b2220cd0731f2b636d0e71
HedyyyChen <1298999619@qq.com>
2025-08-23 09:51:09 +0800

    修改错误代码，符合论文设计

--- models/effnet.py
+++ models/effnet.py
@@ -2,34 +2,34 @@ import torch
 import torch.nn as nn
 
 class EffBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, expansion_rate=4, depth_multiplier=2):
+    def __init__(self, in_channels, out_channels, expand_radio=2):
         super(EffBlock, self).__init__()
         
-        expanded_channels = in_channels * expansion_rate
+        mid_channels = in_channels // expand_radio
         self.expand = nn.Sequential(
-            nn.Conv2d(in_channels, expanded_channels, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(expanded_channels),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(mid_channels),
             nn.LeakyReLU(negative_slope=0.1, inplace=True) 
         )
         
         self.depthwise_1x3 = nn.Sequential(
-            nn.Conv2d(expanded_channels, expanded_channels * depth_multiplier, 
-                     kernel_size=(1, 3), stride=1, padding=(0, 1), groups=expanded_channels, bias=False),
-            nn.BatchNorm2d(expanded_channels * depth_multiplier),
+            nn.Conv2d(mid_channels, mid_channels, 
+                     kernel_size=(1, 3), stride=1, padding=(0, 1), groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels),
             nn.LeakyReLU(negative_slope=0.1, inplace=True)
         )
 
         self.pool_1x2 = nn.MaxPool2d(kernel_size=(1, 2), stride=(1, 2), padding=(0, 0))
         
         self.depthwise_3x1 = nn.Sequential(
-            nn.Conv2d(expanded_channels * depth_multiplier, expanded_channels * depth_multiplier, 
-                     kernel_size=(3, 1), stride=1, padding=(1, 0), groups=expanded_channels * depth_multiplier, bias=False),
-            nn.BatchNorm2d(expanded_channels * depth_multiplier),
+            nn.Conv2d(mid_channels, mid_channels, 
+                     kernel_size=(3, 1), stride=1, padding=(1, 0), groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels),
             nn.LeakyReLU(negative_slope=0.1, inplace=True)
         )
         
         self.project_and_downsample = nn.Sequential(
-            nn.Conv2d(expanded_channels * depth_multiplier, out_channels, 
+            nn.Conv2d(mid_channels, out_channels, 
                      kernel_size=(2, 1), stride=(2, 1), padding=(0, 0), bias=False),
             nn.BatchNorm2d(out_channels)
         )
@@ -55,13 +55,13 @@ class EFFNet(nn.Module):
         self.first_layer = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)  
         )
         
-        self.effnet_block1 = EffBlock(32, 64, expansion_rate=4)  
-        self.effnet_block2 = EffBlock(64, 128, expansion_rate=4)  
-        self.effnet_block3 = EffBlock(128, 256, expansion_rate=4)  
+        self.effnet_block1 = EffBlock(32, 64, expand_radio=2)  
+        self.effnet_block2 = EffBlock(64, 128, expand_radio=2)  
+        self.effnet_block3 = EffBlock(128, 256, expand_radio=2)  
         
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
 
